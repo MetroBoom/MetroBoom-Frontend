51cef05fd9292a1c6f7a3a90336a6d5f68866807
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-03 02:07:46 -0400

    added semi done list without real data

--- App/Components/Lobby.js
+++ App/Components/Lobby.js
@@ -5,77 +5,133 @@ import React, {
   Text,
   Navigator,
   TouchableHighlight,
-  View
+  View,
+  ListView
 } from 'react-native';
 
-class Lobby extends Component {
 
-  changeRoute (row){
+var Lobby  = React.createClass({
+  getInitialState: function () {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+     dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  },
+
+  renderHeader: function () {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.text}>ROOM CODE: GAY420</Text>
+        <TouchableHighlight style={styles.button} onPress={e => {this.changeRoute2(e)}}>
+          <Text style={styles.text2}>+</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  },
+
+  renderRow: function () {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.name}>Song Name</Text>
+        <Text style={styles.artist}>Artist</Text>
+        <Text style={styles.album}>Album</Text>
+      </View>
+    );
+  },
+
+  renderFooter: function () {
+    <View style={styles.footer}>
+      <Text>PlayButton</Text>
+    </View>
+  },
+
+  changeRoute: function (row){
     this.props.navigator.push({
       id: 'JoinRoom',
       name: 'JoinRoom'
     });
-  }
+  },
 
-  changeRoute2 (row){
+  changeRoute2: function (row){
     this.props.navigator.push({
       id: 'Home',
       name: 'Home'
     });
-  }
+  },
 
-  render() {
+  render: function () {
     return (
-      <View style={styles.container}>
-        <Text>Lobby</Text>
-      </View>
+        <ListView
+          style={styles.container}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+          renderHeader={this.renderHeader}
+          renderFooter={this.renderFooter}
+        />
     );
   }
-}
+
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column'
   },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    flex: 2,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginLeft: 10
+  },
+  text2: {
+    color: '#fff',
+    fontSize: 22,
+    flex: 1,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    marginRight: 10
+  },
   header: {
-    flex: 2,
-    marginTop: 100,
-    height: 10,
-    width: 350,
-    resizeMode: 'contain',
-    flexDirection: 'row'
+    flex: 1.5,
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: '#c72a7e'
   },
-  buttons: {
-    flex: 2,
-    flexDirection: 'column',
-    justifyContent: 'center',
+  name: {
+    fontSize: 20,
+    color: '#000',
+    flexDirection: 'column'
   },
-  button: {
-    flex: 1,
-    fontSize: 25,
-    textAlign: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#9238e1',
+  artist: {
+    fontSize: 16,
+    color: '#000',
+    alignSelf: 'center'
+  },
+  album: {
+    fontSize: 16,
+    color: '#000',
+    alignSelf: 'flex-end'
+  },
+  row: {
+    height: 34,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#DDD'
+  },
+  footer: {
+    flex: 1.5,
+    flexDirection: 'row',
+    backgroundColor: '#c72a7e',
     color: '#fff'
   },
-  button2: {
+
+  player: {
     flex: 1,
-    fontSize: 25,
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#b161fc',
-    color: '#fff'
-  },
-  btnText: {
-    flex: 1,
-    fontSize: 25,
-    marginTop: 40,
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    color: '#fff'
+    flexDirection: 'row',
+    backgroundColor: '#c72a7e'
   }
 });
 
